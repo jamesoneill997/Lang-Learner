2ec3b2ac8badc13b2b70cd501a9101bd72fac62c
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-30 08:16:51 +0100

    began working with json

--- translate.py
+++ translate.py
@@ -1,17 +1,27 @@
 from googletrans import Translator
+import json
 
 words = ["lamp", "cup", "fridge", "microwave", "radiator", "blinds", "candle", "window", "beef", "spice"]
 
 def make_translation(words, lang):
     translator = Translator()
+    dictionary = {}
     translation = [translator.translate(word, dest=lang).text for word in words]
 
     for i in range(len(words)):
-        print(words[i] + ' : ' + translation[i])
+        dictionary[words[i]] = translation[i]
 
-    return
+    return dictionary
 #note - add all words to hist.txt
 
-make_translation(words,"es")
-print('\n'*5)
-make_translation(words,"fr")
+es_words = make_translation(words,"es")
+fr_words = make_translation(words,"fr")
+
+def add_to_history(es, fr):
+    with open('es.json', 'w') as file:
+        file.write(json.dumps(es_words))
+
+    with open('fr.json', 'w') as file:
+        file.write(json.dumps(fr_words))
+
+add_to_history(es_words, fr_words)
